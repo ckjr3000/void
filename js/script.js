9133d583fe92916bb90e9d8cb3bcf3fa7446457b
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-06-21 16:21:33 +0100

    Styles interest buttons including VOID trick

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-// Add this to your JavaScript file or within a <script> tag in your HTML file
-document.addEventListener("DOMContentLoaded", function() {
-    const logo = document.querySelector(".logo");
-    const typewriter = document.querySelector(".typewriter h1");
-    const interests = document.querySelector("ul");
-    const instruction = document.querySelector("p");
+// interest button behaviour
 
-    // Initially hide the elements
-    typewriter.style.display = 'none';
-    instruction.style.display = 'none';
-    interests.style.display = 'none';
+const interests = document.getElementsByClassName("interest-btn");
 
-    // Center the logo first
-    setTimeout(() => {
-        logo.classList.add("moved");
-        typewriter.style.display = 'block';
-    }, 1000);
-
-    // Show the typewriter effect and then the interests
-    typewriter.addEventListener('animationend', () => {
-        console.log('yo')
-        instruction.style.display = 'block';
-        interests.style.display = 'block';
+Array.from(interests).forEach(interest => {
+    interest.addEventListener('click', () => {
+        interest.classList.toggle('active');
     });
 });
+
+const decoyBtn = document.getElementById('decoy');
+const voidBtn = document.getElementById('void');
+
+decoyBtn.addEventListener('click', () => {
+    voidBtn.classList.add('active');
+})
