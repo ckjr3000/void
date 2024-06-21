fe306463b2e65fa6cc284a7be757098e93c85b30
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-06-21 16:42:27 +0100

    Adds and styles 'your content is ready' section to homepage

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-// interest button behaviour
-
 const interests = document.getElementsByClassName("interest-btn");
 
 Array.from(interests).forEach(interest => {
     interest.addEventListener('click', () => {
+        console.log('click')
         interest.classList.toggle('active');
     });
 });
 
 const decoyBtn = document.getElementById('decoy');
 const voidBtn = document.getElementById('void');
+const nextPageNav = document.getElementById('next-page');
 
 decoyBtn.addEventListener('click', () => {
     voidBtn.classList.add('active');
+    nextPageNav.classList.remove('hidden');
 })
+
